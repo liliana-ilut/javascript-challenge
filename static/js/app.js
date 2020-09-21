ae81cc148f35c3c18df83987dabe5764ea3801f6
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-09-20 19:34:48 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 // // Variables
-// let button = d3.select("#filter-btn");
-// let inputField1 = d3.select("#datetime");
+let button = d3.select("#filter-btn");
+let inputField1 = d3.select("#datetime");
 // let inputField2 = d3.select("#city");
 // let tbody = d3.select("tbody");
-// let resetbtn = d3.select("#reset-btn");
+let resetbtn = d3.select("#reset-btn");
 // let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 // let populate = (dataInput) => {
@@ -98,3 +98,45 @@ let row = tbody.append("tr");
     cell.text(value);
   });
 });
+
+
+// / Filter by attribute
+button.on("click", () => {
+  d3.event.preventDefault();
+  let inputDate = inputField1.property("value").trim();
+//   let inputCity = inputField2.property("value").toLowerCase().trim();
+  // Filter by field matching input value
+  let filterDate = data.filter(data => data.datetime === inputDate);
+  console.log('date',filterDate)
+//   let filterCity = data.filter(data => data.city === inputCity);
+//   console.log(filterCity)
+//   let filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+//   console.log(filterData)
+
+  // Add filtered sighting to table
+  tbody.html("");
+
+  let response = {
+    // dataUfo, filterCity, 
+    filterDate
+  }
+
+  if (response.dataUfo.length !== 0) {
+    populate(dataUfo);
+  }
+    else if (response.dataUfo.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
+      populate(filterCity) || populate(dataUfo);
+  
+    }
+    else {
+      tbody.append("tr").append("td").text("No results found!"); 
+    }
+})
+
+// resetbtn.on("click", () => {
+//   tbody.html("");
+//   populate(data)
+//   console.log("Table reset")
+// })
+
+ 
