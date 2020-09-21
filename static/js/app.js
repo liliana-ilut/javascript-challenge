5c45da21250d9620609c7e4a51441ed265b82209
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-09-20 22:15:39 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,6 @@
 let button = d3.select("#filter-btn");
 let inputField1 = d3.select("#datetime");
 let tbody = d3.select("tbody");
-// let resetbtn = d3.select("#reset-btn");
 let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 let populate = (dataInput) => {
@@ -37,8 +36,3 @@ button.on("click", () => {
     }
 })
 
-// resetbtn.on("click", () => {
-//   tbody.html("");
-//   populate(data)
-//   console.log("Table reset")
-// })
